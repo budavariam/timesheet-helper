39333ef368bef7154ae69325c03ac02ddcdb5e8b
Mátyás Budavári <budavariam@gmail.com>
2022-01-22 22:18:22 +0100

    Support format empty data

--- src/util/format.ts
+++ src/util/format.ts
@@ -2,8 +2,10 @@ import * as moment from "moment"
 import momentDurationFormatSetup from "moment-duration-format";
 momentDurationFormatSetup(moment);
 
+export const formatAllDuration = (msec: number) => moment
+    .duration(msec, 'milliseconds')
+    .format("hh:mm", { stopTrim: "h" })
+
 export const formatDuration = (msec: number) => msec
-    ? moment
-        .duration(msec, 'milliseconds')
-        .format("hh:mm", { stopTrim: "h" })
+    ? formatAllDuration(msec)
     : ""

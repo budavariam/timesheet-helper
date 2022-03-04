773be92812032530061027d98db91d771f5b3f01
Mátyás Budavári <budavariam@gmail.com>
2022-03-04 20:03:14 +0100

    Make PlaintextData more readable

--- src/project/PlainTextData.tsx
+++ src/project/PlainTextData.tsx
@@ -3,24 +3,29 @@ import { ProjectData } from '../types';
 import { formatAllDuration } from '../util/format';
 import "./PlainTextData.css"
 
-export function PlainTextData(props: any) {
-    const projectData: ProjectData = props.projectData
-    console.log(projectData)
+type PlainTextDataProps = {
+    projectData: ProjectData
+}
+
+export function PlainTextData(props: PlainTextDataProps) {
+    const projectData = props.projectData
+    const csvHeader = [['Client', 'Project', ...projectData.headers.slice(0, -1)].join(",")]
+    const csvBody = projectData.projects
+        .filter(p => !p.ignore)
+        .map((p) => {
+            return [
+                p.client,
+                p.project,
+                p.totals.slice(0, -1).map((val, i) => formatAllDuration(val + p.adjustments[i])),
+            ].join(",")
+        })
+
     return (<>
-    <pre className='plaintext'>
-        {JSON.stringify(projectData.projects
-            .filter(p => !p.ignore)
-            .map((p)=> {
-                return [
-                    p.client, 
-                    p.project, 
-                    p.totals.slice(0, -1).map((val, i) => formatAllDuration(val + p.adjustments[i])),
-                ].join(",")
-        }), null, 2)}
-    </pre>
-    <pre className='plaintext'>
-        {JSON.stringify(projectData.projects, null, 2)}
-    </pre>
-    </>
-    )
+        <pre className='plaintext'>
+            {csvHeader.concat(csvBody).join("\n")}
+        </pre>
+        <pre className='plaintext'>
+            {JSON.stringify(projectData.projects, null, 2)}
+        </pre>
+    </>)
 }
